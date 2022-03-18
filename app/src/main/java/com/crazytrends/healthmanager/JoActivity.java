32ab63eecbdf002a250e04ff995335d80094a55b
josikaprabakaran <mailstojosika@gmail.com>
2022-03-18 12:19:31 +0530

    HELLO

---
+++ app/src/main/java/com/crazytrends/healthmanager/JoActivity.java
@@ -0,0 +1,22 @@
+  package com.crazytrends.healthmanager;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+  public class JoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_jo);
+        Bundle bundle =getIntent().getExtras();
+        if(bundle !=null){
+            if(bundle.getString("some")!=null){
+                Toast.makeText(getApplicationContext(),"data:"+bundle.getString("some"),Toast.LENGTH_SHORT).show();
+            }
+
+        }
+    }
+}
